e96495b1363c6b7911b9758cfa14f9a5bd2e72c6
Huy Hoang <info@huyhoang.nl>
2020-08-14 23:41:20 +0200

    Introducing ReferencingEntity class to hold references to entities

---
+++ src/Dash/src/Dash/Nodes/ReferencingEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace Dash.Nodes
+{
+    public class ReferencingEntity
+    {
+        public ReferencingEntity(Entity entity) : this(entity.Name, entity)
+        {
+        }
+
+        public ReferencingEntity(string name, Entity entity) : this(name, entity, false)
+        {
+        }
+
+        public ReferencingEntity(string name, Entity entity, bool isNullable)
+        {
+            Name = name;
+            Entity = entity;
+            IsNullable = isNullable;
+        }
+
+        public string Name { get; }
+
+        public bool IsNullable { get; }
+
+        public Entity Entity { get; }
+    }
+}
