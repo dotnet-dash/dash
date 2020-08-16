7214374acc6c03207c4c285642c4e97ee7c24789
Huy Hoang <info@huyhoang.nl>
2020-08-16 23:03:51 +0200

    Added unit tests for ErrorRepository

---
+++ src/Dash/test/Dash.Tests/Engine/ErrorRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using Dash.Engine;
+using FluentAssertions;
+using Xunit;
+
+namespace Dash.Tests.Engine
+{
+    public class ErrorRepositoryTests
+    {
+        [Fact]
+        public void HasErrors_NoErrorsAdded_ShouldReturnFalse()
+        {
+            // Arrange
+            var sut = new ErrorRepository();
+
+            // Act
+            var result = sut.HasErrors();
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void HasErrors_ErrorAdded_ShouldReturnTrue()
+        {
+            // Arrange
+            var sut = new ErrorRepository();
+            sut.Add("Error");
+
+            // Act
+            var result = sut.HasErrors();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GetErrors_ErrorAdded_ShouldReturnError()
+        {
+            // Arrange
+            var sut = new ErrorRepository();
+            sut.Add("First error");
+            sut.Add("Second error");
+
+            // Act
+            var result = sut.GetErrors();
+
+            // Assert
+            result.Should().SatisfyRespectively(
+                first =>
+                {
+                    first.Should().Be("First error");
+                },
+                second =>
+                {
+                    second.Should().Be("Second error");
+                }
+            );
+        }
+    }
+}
