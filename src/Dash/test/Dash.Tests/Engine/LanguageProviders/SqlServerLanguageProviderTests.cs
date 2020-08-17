1064a6cbbd4abffdf54dfc9c63cc9810cc9bba27
Huy Hoang <info@huyhoang.nl>
2020-08-17 22:51:55 +0200

    Added unit test

---
+++ src/Dash/test/Dash.Tests/Engine/LanguageProviders/SqlServerLanguageProviderTests.cs
@@ -0,0 +1,29 @@
+﻿using Dash.Engine.LanguageProviders;
+using FluentAssertions;
+using Xunit;
+
+namespace Dash.Tests.Engine.LanguageProviders
+{
+    public class SqlServerLanguageProviderTests
+    {
+        [Theory]
+        [InlineData("Int", "int")]
+        [InlineData("Bool", "bit")]
+        [InlineData("DateTime", "datetime")]
+        [InlineData("Email", "nvarchar")]
+        [InlineData("Guid", "uniqueidentifier")]
+        [InlineData("String", "varchar")]
+        [InlineData("Unicode", "nvarchar")]
+        public void Translate_GivenDashDataType_ShouldReturnExpectedDataType(string dashDataType, string expectedDataType)
+        {
+            // Arrange
+            var sut = new SqlServerLanguageProvider();
+
+            // Act
+            var result = sut.Translate(dashDataType);
+
+            // Assert
+            result.Should().Be(expectedDataType);
+        }
+    }
+}
