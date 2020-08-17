80af6e69693c35a500147e522d1fd466dab95086
Huy Hoang <info@huyhoang.nl>
2020-08-17 22:46:58 +0200

    Added unit test

--- src/Dash/src/Dash/Engine/Generator/DefaultGenerator.cs
+++ src/Dash/src/Dash/Engine/Generator/DefaultGenerator.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.IO.Abstractions;
-using System.Text;
 using System.Threading.Tasks;
 using Dash.Engine.Abstractions;
 using Dash.Engine.Models.SourceCode;
@@ -13,15 +11,18 @@ namespace Dash.Engine.Generator
         private readonly ITemplateProvider _templateProvider;
         private readonly IFileSystem _fileSystem;
         private readonly IModelRepository _modelRepository;
+        private readonly IConsole _console;
 
         public DefaultGenerator(
             ITemplateProvider templateProvider,
             IFileSystem fileSystem,
-            IModelRepository modelRepository)
+            IModelRepository modelRepository,
+            IConsole console)
         {
             _templateProvider = templateProvider;
             _fileSystem = fileSystem;
             _modelRepository = modelRepository;
+            _console = console;
         }
 
         public async Task Generate(SourceCodeDocument model)
@@ -41,9 +42,9 @@ namespace Dash.Engine.Generator
                 );
 
                 var path = Path.Combine(templateNode.Output, $"{templateNode.Template!}.generated.cs");
-                Console.Out.WriteLine($"Writing to file {path}");
+                _console.Info($"Generating file {path}");
 
-                await _fileSystem.File.WriteAllTextAsync(path, output, Encoding.UTF8);
+                await _fileSystem.File.WriteAllTextAsync(path, output);
             }
         }
     }
