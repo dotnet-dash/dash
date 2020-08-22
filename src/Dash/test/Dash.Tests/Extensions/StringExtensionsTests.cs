09ee41d3d0e6264355e35d7097db6b60ab24a0fa
Huy Hoang <info@huyhoang.nl>
2020-08-22 23:38:56 +0200

    Added new extension methods and unit tests

--- src/Dash/test/Dash.Tests/Extensions/StringExtensionsTests.cs
+++ src/Dash/test/Dash.Tests/Extensions/StringExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using Dash.Extensions;
 using FluentAssertions;
+using System;
 using Xunit;
 
 namespace Dash.Tests.Extensions
@@ -18,6 +19,50 @@ namespace Dash.Tests.Extensions
 
             // Assert
             result.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public void IsValidUri_UriIsValid_ShouldReturnTrue()
+        {
+            // Act
+            var result = "https://www.foo.bar".IsValidUri();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsValidUri_UriIsInvalid_ShouldReturnFalse()
+        {
+            // Act
+            var result = "foobar".IsValidUri();
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("invalid")]
+        public void ToUri_InvalidUri_ShouldReturnNull(string invalidUri)
+        {
+            // Act
+            var result = invalidUri.ToUri();
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("https://www.foo.com")]
+        [InlineData("file://localhost")]
+        public void ToUri_ValidUri_ShouldReturnUri(string validUri)
+        {
+            // Act
+            var result = validUri.ToUri();
+
+            // Assert
+            result.Should().Be(new Uri(validUri));
         }
     }
 }
