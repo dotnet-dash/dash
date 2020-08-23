9c579b1fb4c01fd3675812ad9f97ff7ec57b6fb4
Huy Hoang <info@huyhoang.nl>
2020-08-24 00:20:52 +0200

    Remove unnecessary 'using'

--- src/Dash/src/Dash/Nodes/ConfigurationNode.cs
+++ src/Dash/src/Dash/Nodes/ConfigurationNode.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dash.Engine;
-using Dash.Extensions;
 
 namespace Dash.Nodes
 {
