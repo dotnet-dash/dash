22c3cf4deec0ffeb817cf12b146d59d3512fdb91
Huy Hoang <info@huyhoang.nl>
2020-08-27 21:26:02 +0200

    Remove unnecessary 'using'

--- src/Dash/src/Dash/Application/ApplicationServiceProvider.cs
+++ src/Dash/src/Dash/Application/ApplicationServiceProvider.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Huy Hoang. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dash.Application
