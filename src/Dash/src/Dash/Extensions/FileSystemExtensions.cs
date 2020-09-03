a864c7f8ec9e28f9f7667999d8cb51228b005b2c
Huy Hoang <info@huyhoang.nl>
2020-09-03 18:33:42 +0200

    Add extension method

---
+++ src/Dash/src/Dash/Extensions/FileSystemExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Abstractions;
+using System.Linq;
+
+namespace Dash.Extensions
+{
+    public static class FileSystemExtensions
+    {
+        public static string? GetNearestEditorConfig(this IFileSystem fileSystem, string path)
+        {
+            var directoryInfo = fileSystem.DirectoryInfo.FromDirectoryName(path);
+            while (directoryInfo != null)
+            {
+                var files = fileSystem.Directory.GetFiles(directoryInfo.FullName, ".editorconfig");
+                if (files.Any())
+                {
+                    return files.Single();
+                }
+
+                var projectFiles = fileSystem.Directory.GetFiles(directoryInfo.FullName, "*.csproj");
+                if (projectFiles.Any())
+                {
+                    return null;
+                }
+
+                directoryInfo = directoryInfo.Parent;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Dash/test/Dash.Tests/Extensions/FileSystemExtensionsTests.cs
@@ -0,0 +1,86 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using Dash.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Dash.Tests.Extensions
+{
+    public class FileSystemExtensionsTests
+    {
+        private readonly MockFileData _foo = MockFileData.NullObject;
+
+        [Fact]
+        public void GetNearestEditorConfig_NoEditorConfigInCurrentDirectory_ShouldReturnNull()
+        {
+            // Arrange
+            var sut = new MockFileSystem();
+
+            // Act
+            var result = sut.GetNearestEditorConfig("c:/temp");
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetNearestEditorConfig_EditorConfigFoundInCurrentDirectory_ShouldReturnEditorConfigPath()
+        {
+            // Arrange
+            var sut = new MockFileSystem();
+            sut.AddFile("c:/temp/.editorconfig", _foo);
+
+            // Act
+            var result = sut.GetNearestEditorConfig("c:/temp");
+
+            // Assert
+            result.Should().Be(@"C:\temp\.editorconfig");
+        }
+
+        [Fact]
+        public void GetNearestEditorConfig_NoEditorConfigFoundInAncestorDirectories_ShouldReturnNull()
+        {
+            // Arrange
+            var sut = new MockFileSystem();
+            sut.AddFile("c:/temp/.editorconfig", _foo);
+            sut.AddFile("c:/temp/project/myproject.csproj", _foo);
+            sut.AddFile("c:/temp/project/controllers/foo.cs", _foo);
+
+            // Act
+            var result = sut.GetNearestEditorConfig("c:/temp/project/controllers");
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetNearestEditorConfig_EditorConfigFoundInParentDirectory_ShouldReturnEditorConfigPath()
+        {
+            // Arrange
+            var sut = new MockFileSystem();
+            sut.AddFile("c:/temp/project/.editorconfig", _foo);
+            sut.AddFile("c:/temp/project/controllers/foo.cs", _foo);
+
+            // Act
+            var result = sut.GetNearestEditorConfig("c:/temp/project/controllers");
+
+            // Assert
+            result.Should().Be(@"C:\temp\project\.editorconfig");
+        }
+
+        [Fact]
+        public void GetNearestEditorConfig_MultipleEditorConfigInProjectDirectory_ShouldReturnNearest()
+        {
+            // Arrange
+            var sut = new MockFileSystem();
+            sut.AddFile("c:/temp/project/.editorconfig", _foo);
+            sut.AddFile("c:/temp/project/controllers/foo.cs", _foo);
+            sut.AddFile("c:/temp/project/controllers/.editorconfig", _foo);
+
+            // Act
+            var result = sut.GetNearestEditorConfig("c:/temp/project/controllers");
+
+            // Assert
+            result.Should().Be(@"C:\temp\project\controllers\.editorconfig");
+        }
+    }
+}
