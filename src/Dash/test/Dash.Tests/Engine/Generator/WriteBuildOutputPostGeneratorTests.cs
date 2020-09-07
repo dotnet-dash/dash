dff97fcb8d7af5d18aada326b94190feff9b0af0
Huy Hoang <info@huyhoang.nl>
2020-09-07 23:51:14 +0200

    Increased code coverage

---
+++ src/Dash/test/Dash.Tests/Engine/Generator/WriteBuildOutputPostGeneratorTests.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Huy Hoang. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Threading.Tasks;
+using Dash.Common;
+using Dash.Engine;
+using Dash.Engine.Generator;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Dash.Tests.Engine.Generator
+{
+    public class WriteBuildOutputPostGeneratorTests
+    {
+        private readonly IConsole _console = Substitute.For<IConsole>();
+        private readonly IBuildOutputRepository _buildOutputRepository = Substitute.For<IBuildOutputRepository>();
+        private readonly MockFileSystem _fileSystem = new MockFileSystem();
+        private readonly WriteBuildOutputPostGenerator _sut;
+
+        public WriteBuildOutputPostGeneratorTests()
+        {
+            _sut = new WriteBuildOutputPostGenerator(_console, _buildOutputRepository, _fileSystem);
+        }
+
+        [Fact]
+        public async Task Run_NoBuildOutputs_ShouldWriteNothing()
+        {
+            // Act
+            await _sut.Run();
+
+            // Assert
+            _fileSystem.AllFiles.Count().Should().Be(0);
+        }
+
+        [Fact]
+        public async Task Run_BuildOutputToExistingPath_ShouldWriteToFileSystem()
+        {
+            // Assert
+            var buildOutputs = new List<BuildOutput>
+            {
+                new BuildOutput("c:/foo.cs", "Foo")
+            };
+
+            _buildOutputRepository.GetOutputItems().Returns(buildOutputs);
+
+            // Act
+            await _sut.Run();
+
+            // Assert
+            _fileSystem.GetFile("c:/foo.cs").TextContents.Should().Be("Foo");
+        }
+
+        [Fact]
+        public async Task Run_BuildOutputToNonExistingPath_ShouldWriteToFileSystem()
+        {
+            // Assert
+            var buildOutputs = new List<BuildOutput>
+            {
+                new BuildOutput("c:/foo/bar.cs", "Foobar")
+            };
+
+            _buildOutputRepository.GetOutputItems().Returns(buildOutputs);
+
+            // Act
+            await _sut.Run();
+
+            // Assert
+            _fileSystem.GetFile("c:/foo/bar.cs").TextContents.Should().Be("Foobar");
+        }
+    }
+}
---
+++ src/Dash/test/Dash.Tests/Engine/Repositories/BuildOutputRepositoryTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Dash.Engine;
+using Dash.Engine.Repositories;
+using FluentAssertions;
+using Xunit;
+
+namespace Dash.Tests.Engine.Repositories
+{
+    public class BuildOutputRepositoryTests
+    {
+        [Fact]
+        public void Add_NotAdded_ShouldAdd()
+        {
+            // Arrange
+            var sut = new BuildOutputRepository();
+
+            // Act
+            sut.Add("c:/foo.cs", "Foo");
+
+            // Assert
+            sut.GetOutputItems().Should().SatisfyRespectively(first =>
+            {
+                first.Path.Should().Be("c:/foo.cs");
+                first.GeneratedSourceCodeContent.Should().Be("Foo");
+            });
+        }
+
+        [Fact]
+        public void Add_Added_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            var sut = new BuildOutputRepository();
+            sut.Add("c:/foo.cs", "Foo");
+
+            // Act
+            Action act = () => sut.Add("c:/foo.cs", "Foobar");
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Update_NotAdded_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            var sut = new BuildOutputRepository();
+
+            // Act
+            Action act = () => sut.Update(new BuildOutput("c:/foo.cs", "Bar"));
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Update_Added_ShouldUpdate()
+        {
+            // Arrange
+            var sut = new BuildOutputRepository();
+            sut.Add("c:/foo.cs", "Foo");
+
+            // Act
+            sut.Update(new BuildOutput("c:/foo.cs", "Bar"));
+
+            // Assert
+            sut.GetOutputItems().Should().SatisfyRespectively(first =>
+            {
+                first.Path.Should().Be("c:/foo.cs");
+                first.GeneratedSourceCodeContent.Should().Be("Bar");
+            });
+        }
+
+        [Fact]
+        public void GetOutputItems_MultipleItems_ShouldReturnItems()
+        {
+            // Arrange
+            var sut = new BuildOutputRepository();
+            sut.Add("c:/foo.cs", "Foo");
+            sut.Add("c:/bar.cs", "Bar");
+
+            // Act
+            var result = sut.GetOutputItems();
+
+            // Assert
+            result.Should().SatisfyRespectively(first =>
+            {
+                first.Path.Should().Be("c:/foo.cs");
+                first.GeneratedSourceCodeContent.Should().Be("Foo");
+            }, second =>
+            {
+                second.Path.Should().Be("c:/bar.cs");
+                second.GeneratedSourceCodeContent.Should().Be("Bar");
+            });
+        }
+    }
+}
