508779a8bb84c4b384790398e9e80afa7bc800c7
Huy Hoang <info@huyhoang.nl>
2020-09-07 23:18:47 +0200

    Added missing headers

--- src/Dash/test/Dash.Tests/Application/Default/DashOptionsValidatorTests.cs
+++ src/Dash/test/Dash.Tests/Application/Default/DashOptionsValidatorTests.cs
@@ -1,4 +1,7 @@
-﻿using System.IO.Abstractions.TestingHelpers;
+﻿// Copyright (c) Huy Hoang. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.IO.Abstractions.TestingHelpers;
 using System.Threading.Tasks;
 using Dash.Application;
 using Dash.Application.PreprocessingSteps;
--- src/Dash/test/Dash.Tests/TestHelpers/FluentAssertionsExtensions.cs
+++ src/Dash/test/Dash.Tests/TestHelpers/FluentAssertionsExtensions.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿// Copyright (c) Huy Hoang. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using FluentAssertions;
 using FluentAssertions.Execution;
 using FluentAssertions.Primitives;
 using Microsoft.CodeAnalysis.CSharp;
