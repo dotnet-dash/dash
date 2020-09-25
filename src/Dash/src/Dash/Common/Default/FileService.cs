c868646c4f7cbf2bb7ee09eb924a5ad51c561992
Huy Hoang <info@huyhoang.nl>
2020-09-26 00:03:38 +0200

    Add header

--- src/Dash/src/Dash/Common/Default/FileService.cs
+++ src/Dash/src/Dash/Common/Default/FileService.cs
@@ -1,4 +1,7 @@
-﻿using System.IO.Abstractions;
+﻿// Copyright (c) Huy Hoang. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.IO.Abstractions;
 using Dash.Application;
 using Dash.Extensions;
 using Microsoft.Extensions.Options;
